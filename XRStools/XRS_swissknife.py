d690d8816880c5a5b257eca3432d39743aac4848
Alessandro MIRONE <alex@lapalex>
2015-04-30 00:19:10 +0200

    Documentation

--- XRStools/XRS_swissknife.py
+++ XRStools/XRS_swissknife.py
@@ -1,3 +1,44 @@
+__doc__="""
+Documentation of XRS_swissknife
+-------------------------------
+
+The script is called in this way ::
+
+  XRS_swissknife yourinput.yaml
+
+The input file is in *yaml* format. In *yaml* format each line introduces an item
+and the indentation expresses the hierarchy.
+An example is ::
+
+  Fancy_Reduction :
+        parameterTom : 3.14
+        parameterJerry : False
+        choicesBob     : [1,2,3]
+
+In this example we create an item called *Fancy_Reduction* which contains three subitems.
+
+The *XRS_swissknife* expects that for each operation that you want to apply, you provide an item
+named as the operation, and the associated subitems that provide that values for the parameters.
+
+*XRS_swissknife* will do what you want provided you respect the proper indentation. A thing which helps
+is using emacs and activating the *python mode*, because python uses the same indentation principle
+to structure the code. 
+
+Each processing item has the additional, optional, key **active**.
+This key can be set to **0** or **1** to desactivate or not (default is **1**, active) the processing.
+Here a desactivation example ::
+
+  Fancy_Reduction :
+        active : 0
+        parameterTom : 3.14
+        parameterJerry : False
+        choicesBob     : [1,2,3]
+
+The following documentation has been created automatically, for each functionality, based on the documentation string
+written in the code for the fonctionality itself.  You can also write mathematical expressions :  :math:`\\int{ x dx} = \\frac { x^2 }{ 2 }`
+and even include graphics.
+
+"""
 import string
 from yaml import load, dump
 try:
@@ -30,7 +71,22 @@ def main():
 
 def help(yamlData):
     """
-    Displays all operations and their doc
+    **help**
+
+    Displays doc on the operations. In the input file ::
+
+       help :
+
+    will trigger printing of all the available operation names ::
+
+       help :
+           create_rois
+           load_scans
+
+    will print the help on *create_rois* and the help about *load_scans*.
+    By the way, it is the same that you can read here because the *sphinx* doc-generation tool
+    reads the same docstrings contained in the code.
+     
     """
     print " HELP " *15
     if yamlData ["help"] is None:
@@ -51,42 +107,44 @@ def help(yamlData):
 
 def Extraction(yamlData):
     """ 
+    **Extraction**
+
     Function to extract the interesting signal after removal of Compton profile,
     linear baselines,  Pearson profile
-    Example :
+    Example ::
 
-    active : 1
-    dataadress   : "pippo.h5:/ROI_A/loaded_datas"         # where load_scans wrote data
-    HFaddress    : "pippo.h5:/ROI_A/loaded_datas/HF_O"    # where compton profiles have been calculated
-    # prenormrange : [ 5 , .inf ]		
+     active : 1
+     dataadress   : "pippo.h5:/ROI_A/loaded_datas"         # where load_scans wrote data
+     HFaddress    : "pippo.h5:/ROI_A/loaded_datas/HF_O"    # where compton profiles have been calculated
+     # prenormrange : [ 5 , .inf ]		
 
-    analyzerAverage :                                     #  averaging over analysers
-        active : 1
-        which : [0,11  , 36,59   ]
-        errorweighing  : False
+     analyzerAverage :                                     #  averaging over analysers
+         active : 1
+         which : [0,11  , 36,59   ]
+         errorweighing  : False
 
-    removeLinearAv :                                      #  fit a linear baseline and remove it
-        active  : 1
-        region1 :  [520.0,532.0]   
-        region2 :  None 
-        ewindow : 100 
-        scale : 1
+     removeLinearAv :                                      #  fit a linear baseline and remove it
+         active  : 1
+         region1 :  [520.0,532.0]   
+         region2 :  None 
+         ewindow : 100 
+         scale : 1
 
-    removePearsonAv:                                      # fit a Pearson and remove it
-        active  : 0
-        region1 :  [520.0,532.0]   
-        region2 :  None  
-        guess :
-            Peak_position : 600.0
-            FWHM          :  10
-            Shape         : "Lorentzian" 
-            Peak_intensity: 100.0
-            linear_slope  : 1
-            linear_background : 0
-            scaling_factor : 1
+     removePearsonAv:                                      # fit a Pearson and remove it
+         active  : 0
+         region1 :  [520.0,532.0]   
+         region2 :  None  
+         guess :
+             Peak_position : 600.0
+             FWHM          :  10
+             Shape         : "Lorentzian" 
+             Peak_intensity: 100.0
+             linear_slope  : 1
+             linear_background : 0
+             scaling_factor : 1
             
-    view   :   0
-    target :   "myextraction"                            # path relative to dataadress where extracted signal will be written
+     view   :   0
+     target :   "myextraction"                            # path relative to dataadress where extracted signal will be written
 
     """
 
@@ -139,17 +197,19 @@ but : was not found
 
 def  HFspectrum(yamlData):
     """
-		function for building S(q,w) from tabulated Hartree-Fock Compton profiles to use in
-		the extraction algorithm.
+    **HFspectrum**
 
-    EXAMPLE :
+    function for building S(q,w) from tabulated Hartree-Fock Compton profiles to use in
+    the extraction algorithm.
 
-    dataadress : "hdf5filename:full_nameof_signals_group"   # where load_scans wrote data
-    formulas   :  ['O']     # list of strings of chemical sum formulas of which the sample is made up
-    concentrations : [1.0]  # list of concentrations of how the different chemical formulas are mixed (sum should be 1)
-    correctasym    : [[0.0,0.0,0.0]]  #  single value or list of scaling values for the HR-correction to 
-			              # the 1s, 2s, and 2p shells. one value per element in the list of formulas
-    hfspectrum_address : "nameofgroup" # Target group for writing Relative to dataadress (and in the same file)!!!!
+    EXAMPLE ::
+
+      dataadress : "hdf5filename:full_nameof_signals_group"   # where load_scans wrote data
+      formulas   :  ['O']     # list of strings of chemical sum formulas of which the sample is made up
+      concentrations : [1.0]  # list of concentrations of how the different chemical formulas are mixed (sum should be 1)
+      correctasym    : [[0.0,0.0,0.0]]  #  single value or list of scaling values for the HR-correction to 
+          		              # the 1s, 2s, and 2p shells. one value per element in the list of formulas
+      hfspectrum_address : "nameofgroup" # Target group for writing Relative to dataadress (and in the same file)!!!!
 
  
     """
@@ -171,32 +231,35 @@ def  HFspectrum(yamlData):
 
 def load_scans(yamlData):
     """
+    **load_scans**
+
     This command harvest the selected signals.
-    the instructions on the scans to be taken must be in the form( as submembers ofload_scans ) :
+    the instructions on the scans to be taken must be in the form( as submembers ofload_scans ) ::
+
+     roiaddress :  "hdf5filename:nameofroigroup"  # the same given in create_rois
+     expdata    :  "absolutepathtoaspecfile"  # this points to a spec file
+  
+     elastic_scans    : [623]
+     fine_scans       : [626,630,634,638,642]
+     n_loop           : 4
+     long_scan        : 624
+  
+     signaladdress : "nameofsignalgroup"  # Target group for writing Relative to ROI (and in the same file)!!!!
+
+     #############################################################
+     # OPTIONALS
+     #
+     order : [0,1,2,3,4,5]  #  list of integers (0-5) which describes the order of modules in which the 
+                            #	ROIs were defined (default is VD, VU, VB, HR, HL, HB; i.e. [0,1,2,3,4,5])
+
+     rvd : -41              # mean tth angle of HL module (default is 0.0)
+     rvu : 85               # mean tth angle of HR module (default is 0.0)
+     rvb : 121.8            # mean tth angle of HB module (default is 0.0)
+     rhl : 41.0             # mean tth angle of VD module (default is 0.0)
+     rhr : 41.0             # mean tth angle of VU module (default is 0.0)
+     rhb : 121.8            # mean tth angle of VB module (default is 0.0)
 
 
-    roiaddress :  "hdf5filename:nameofroigroup"  # the same given in create_rois
-    expdata    :  "absolutepathtoaspecfile"  # this points to a spec file
-
-    elastic_scans    : [623]
-    fine_scans       : [626,630,634,638,642]
-    n_loop           : 4
-    long_scan        : 624
-
-    signaladdress : "nameofsignalgroup"  # Target group for writing Relative to ROI (and in the same file)!!!!
-
-    #############################################################
-    # OPTIONALS
-    #
-    order : [0,1,2,3,4,5]  #  list of integers (0-5) which describes the order of modules in which the 
-			   #	ROIs were defined (default is VD, VU, VB, HR, HL, HB; i.e. [0,1,2,3,4,5])
-
-    rvd : -41              # mean tth angle of HL module (default is 0.0)
-    rvu : 85               # mean tth angle of HR module (default is 0.0)
-    rvb : 121.8            # mean tth angle of HB module (default is 0.0)
-    rhl : 41.0             # mean tth angle of VD module (default is 0.0)
-    rhr : 41.0             # mean tth angle of VU module (default is 0.0)
-    rhb : 121.8            # mean tth angle of VB module (default is 0.0)
     #
     """
     mydata = yamlData["load_scans"]
@@ -266,36 +329,41 @@ def gvord(yamldata, key, default=None):
 
 def create_rois(yamlData):
     """
+    **create_rois**
+
     This launches the roi selector.
     If yamlData contains instructions on the scan to be taken , the roi 
     selector will pop up with the image ready to be used. Otherwise you have to
     browse some file around.
 
-    At the end you have the possibility to write the rois on a container hdf5 file
+    At the end you have the possibility to write the rois on a container hdf5 file.
 
-    the instructions on the scans to be taken must be in the form( as submembers of create_rois) :
+    In the extreme case when you give no argument ( parameters) ::
+
+        create_rois :
+
+    The roi selector window  pops up and you have to select an image at the beginning
+    and select a hdf5 file, and a name of a node inside the file, where the rois 
+    will be written.
+
+    You can  :
 
     expdata :  "absolutepathtoaspecfile"  # this points to a spec file
     scans   : [623,624]                   # a list containing one or more scans 
                                           # for the elastic part.
                                           # They will be summed to an image
                                           # and rois will then be extracted from this image.
-    
+    roiaddress : "myfile.hdf5:/path/to/hdf5/group"  # the target destination for rois
+   
     If expdata is not given in the yaml input file, then the roicreation widget will be launched
     without data, and you have to open some file for the image.
 
-    Before quitting the roiwidget you have the possibility to write the rois into a hdf5 file.
+    If roiaddress is not given,  before quitting the roiwidget you have the possibility to write the rois into a hdf5 file.
     You do this by choosing a filename for the hdf5 file, and a name for the roi selection.
     An entry, with this matter name, will be created into the hdf5 file ( all subsequent
     treatements, done by other methods than this, which uses this roi selection 
     ( called by its name )  will be reported into subentries of such entry)
 
-    You can also decide the target entry already in the yaml file, using such from :
-
-    roiaddress : "myfile.hdf5:/path/to/hdf5/group"
-
-    so that writing will be done in file myfile.hdf5 at the group /path/to/hdf5/group
- 
     """
     mydata = yamlData["create_rois"]
     if mydata is not None and mydata.has_key("active") :
---
+++ doc/invocation.rst
@@ -0,0 +1,12 @@
+Code Invocation
+===============
+  * Some of the XRStools capabilities can be accessed by invocation of the *XRS_swissknife* script, providing as 
+    input a file in the *yaml* format.
+
+  * Depending on the details of your installation, you have the *XRS_swissknife* script sitting somewhere in a directory. Check the *Installation* page to see how to set PYTHONPATH and PATH in case of a local installation.
+
+    *The following documentation has been generated automatically from the comments found in the code*.
+ 
+.. automodule:: XRS_swissknife
+    :members: help, create_rois,load_scans,Extraction
+		 
