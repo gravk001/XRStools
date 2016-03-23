gromacs files in calctools, different pre-normalization in extraction, smaller bugs fixed

XRStools/xrs_calctools.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-# Filename: stobe_analyze.py
+# Filename: xrs_calctools.py
 
+import xrs_utilities
 import os
 import warnings
 from copy import deepcopy
@@ -516,18 +517,19 @@ class xyzAtom:
 			print('Vector must be 3D np.array!') 
 
 class xyzMolecule:
-        def __init__(self,xyzAtoms):
-                self.xyzAtoms = xyzAtoms
+	def __init__(self,xyzAtoms):
+		self.xyzAtoms = xyzAtoms
+		self.title = None
 
-        def getCoordinates(self):
-                return [atom.getCoordinates() for atom in self.xyzAtoms]
+	def getCoordinates(self):
+		return [atom.getCoordinates() for atom in self.xyzAtoms]
 
-        def getCoordinates_name(self,name):
-                atoms = []
-                for atom in self.xyzAtoms:
-                        if atom.name == name:
-                                atoms.append(atom.coordinates)
-                return atoms
+	def getCoordinates_name(self,name):
+		atoms = []
+		for atom in self.xyzAtoms:
+			if atom.name == name:
+				atoms.append(atom.coordinates)
+		return atoms
 
         def get_atoms_by_name(self,name):
                 atoms = []
@@ -561,6 +563,21 @@ class xyzMolecule:
 		ax.scatter(x_vals, y_vals, z_vals)
 		show()
 
+	def appendAtom(self,Atom):
+		if isinstance(Atom,xyzAtom):
+			self.xyzAtoms.append(Atom)
+		elif isinstance(Atom,list):
+			self.xyzAtoms.extend(Atom)
+
+	def popAtom(self,xyzAtom):
+		self.xyzAtoms.remove(xyzAtom)
+
+	def writeXYZfile(self,fname):
+		if not self.title:
+			self.title = 'None'
+		writeXYZfile(fname, len(self.xyzAtoms), self.title, self.xyzAtoms)
+
+
 class xyzBox:
         def __init__(self,xyzAtoms,boxLength=None):
                 self.xyzMolecules = []
@@ -575,42 +592,125 @@ class xyzBox:
 		else:
 			self.boxLength = boxLength*constants.physical_constants['atomic unit of length'][0]*10**10
 
-    def writeBox(self, filename):
-        writeXYZfile(filename, self.n_atoms, self.title, self.xyzAtoms)
+	def writeBox(self, filename):
+		writeXYZfile(filename, self.n_atoms, self.title, self.xyzAtoms)
 
-    def writeRelBox(self,filename,inclAtomNames=True):
-        if not self.boxLength:
-            print('Cannot write rel. coordinates without boxLength. Need to set it first.')
-            return
-        else:
-            writeRelXYZfile(filename, self.n_atoms, self.boxLength, self.title, self.xyzAtoms, inclAtomNames)
+	def writeRelBox(self,filename,inclAtomNames=True):
+		if not self.boxLength:
+			print('Cannot write rel. coordinates without boxLength. Need to set it first.')
+			return
+		else:
+			writeRelXYZfile(filename, self.n_atoms, self.boxLength, self.title, self.xyzAtoms, inclAtomNames)
 
-    def multiplyBoxPBC(self,range=[-1,1]):
-        # copy box, translate by boxLength into all directions, append to current box
-        pass
-            
-    def getCoordinates(self):		
-        return [atom.getCoordinates() for atom in self.xyzAtoms]
+	def multiplyBoxPBC(self,numShells):
+		if not self.boxLength:
+			print('Cannot multiply without boxLength. Need to set it first.')
+			return
+		all_atoms = getPeriodicTestBox(xyzAtoms,boxLength,numbershells=numShells)
+		self.xyzMolecules = []
+		self.xyzAtoms     = all_atoms
+		self.n_atoms      = self.n_atoms*(numShells*2.+1.)**3.
+		self.boxLength    = self.boxLength*(numShells*2.0+1.0)
 
-    def get_atoms_by_name(self,name):
-        # find oxygen atoms
-        atoms = []
-        for atom in self.xyzAtoms:
-                if atom.name == name:
-                        atoms.append(atom)
-                else:
-                        pass
-        if len(atoms) == 0:
-                print('Found no atoms with given name in box.')
-                return
-        return atoms
+	def deleteTip4pCOM(self):
+		for atom in self.xyzAtoms:
+			if atom.name == 'M':
+				self.xyzAtoms.remove(atom)
+				self.n_atoms -= 1
+
+	def writeClusters(self,cenatom_name, number,cutoff,prefix,postfix='.xyz'):
+		""" **writeXYZclusters**
+		Write water clusters into files.
+		"""
+		# find central atom
+		cen_atom = self.get_atoms_by_name(cenatom_name)[number]
+		coor1 = cen_atom.getCoordinates()
+		# cut clusters and write files
+		atoms = []
+		atoms.append(cen_atom)
+		for atom2 in self.xyzAtoms:
+			coor2 = atom2.getCoordinates()
+			if np.linalg.norm( coor1 - coor2) > 0.0 and np.linalg.norm( coor1 - coor2) <= cutoff:
+				atoms.append(atom2)
+		fname = prefix + '_%03d' % number + postfix
+		box = xyzBox(atoms)
+		box.writeBox(fname)
+
+	def writeH2Oclusters(self,cutoff,prefix,postfix='.xyz',o_name='O',h_name='H'):
+		""" **writeXYZclusters**
+		Write water clusters into files.
+		"""
+		if not self.boxLength:
+			print('Cannot multiply without boxLength. Need to set it first.')
+			return
+		# find H2O molecules
+		self.get_h2o_molecules(o_name,h_name)
+		# get a test box
+		pbcMols = getPeriodicTestBox_molecules(self.xyzMolecules,self.boxLength,numbershells=1)
+		# cut clusters and write files
+		for o_atom, ii in zip(o_atoms,range(len(o_atoms))):
+			cluster = []
+			for molecule in pbcMols:
+				coor = molecule.getCoordinates_name(o_name)
+				if np.linalg.norm( o_atom.coordinates - coor) <= cutoff:
+					cluster.extend(molecule.xyzAtoms)
+			fname = prefix + '_%03d' % ii + postfix
+			box = xyzBox(cluster)
+			box.writeBox(fname)
+
+	def writeMoleculeCluster(self,molAtomList,fname,cutoff=None,numH2Omols=None,o_name='O',h_name='H',mol_center=None):
+		""" **writeMoleculeCluster**
+		Careful, this works only for a single molecule in water.
+		"""
+		if not self.boxLength:
+			print('Cannot multiply without boxLength. Need to set it first.')
+			return
+		# find H2O molecules
+		self.get_h2o_molecules(o_name,h_name)
+		# get a test box
+		pbcMols = getPeriodicTestBox_molecules(self.xyzMolecules,self.boxLength,numbershells=1)
+		# find the solute molecule
+		cluster = findMolecule(self.xyzAtoms,molAtomList)
+		# find center of mass of molecule
+		if not mol_center:
+			cenom = cluster.getGeometricCenter()
+		else:
+			cenom = cluster.getCoordinates_name(mol_center)
+		if cutoff:
+			# use cutoff criterium
+			waters = findAllWaters(cenom,pbcMols,o_name,cutoff)
+			cluster.appendAtom(waters)
+		elif numH2Omols:
+			# use number of waters to include
+			dists = getDistsFromMolecule(cenom,pbcMols,o_name=o_name)
+			inds  = np.argsort(dists)
+			for ii in range(numH2Omols):
+				cluster.appendAtom(pbcMols[inds[ii]].xyzAtoms)
+		else:
+			print('Something is fishy!')
+			return
+		cluster.writeXYZfile(fname)
+
+	def getCoordinates(self):		
+		return [atom.getCoordinates() for atom in self.xyzAtoms]
+
+	def get_atoms_by_name(self,name):
+		# find oxygen atoms
+		atoms = []
+		for atom in self.xyzAtoms:
+			if atom.name == name:
+				atoms.append(atom)
+			else:
+				pass
+		if len(atoms) == 0:
+			print('Found no atoms with given name in box.')
+			return
+		return atoms
 
 	def scatterPlot(self):
 		from mpl_toolkits.mplot3d import Axes3D
 		fig = figure()
 		ax  = Axes3D(fig)
-		#coordinates = self.getCoordinates()
-		#print type(coordinates), coordinates
 		x_vals = [coord[0] for coord in self.getCoordinates()]
 		y_vals = [coord[1] for coord in self.getCoordinates()]
 		z_vals = [coord[2] for coord in self.getCoordinates()]
@@ -685,9 +785,66 @@ class xyzBox:
 			for atom in mol.xyzAtoms:
 				self.xyzAtoms.append(atom)
 
-    def getTetraParameter(self):
-        pass
-            
+	def getTetraParameter(self):
+		if not self.boxLength:
+			print('This only works with PBC. Need to set boxLength first.')
+			return
+		else:
+			o_atoms  = self.get_atoms_by_name('O')
+			return getTetraParameter(o_atoms,self.boxLength)
+
+
+class xyzTrajectory:
+	def __init__(self,xyzBoxes):
+		self.xyzBoxes = xyzBoxes
+
+	def writeRandBox(self,filename):
+		ind = np.random.randint(len(self.xyzBoxes))
+		self.xyzBoxes[ind].writeBox(filename)
+
+
+
+
+
+def getDistsFromMolecule(point,listOfMolecules,o_name=None):
+	dists = []
+	if not o_name:
+		for mol in listOfMolecules:
+			dists.append(np.linalg.norm(point - mol.getGeometricCenter()))
+		return dists
+	else:
+		for mol in listOfMolecules:
+			coor = mol.getCoordinates_name(o_name)[0]
+			dists.append(np.linalg.norm(point - coor))
+		return dists
+			
+def findAllWaters(point,waterMols,o_name,cutoff):
+	atoms = []
+	for mol in waterMols:
+		coor = mol.getCoordinates_name(o_name)
+		if np.linalg.norm( point - coor) <= cutoff:
+			atoms.extend(mol.xyzAtoms)
+	return atoms
+
+def findMolecule(xyzAtoms,molAtomList):
+	molecule = []
+	for atom in xyzAtoms:
+		if atom.name in molAtomList:
+			molecule.append(atom)
+	return xyzMolecule(molecule)
+
+def calculateCOMlist(atomList):
+	""" **calculateCOMlist**
+	Calculates center of mass for a list of atoms.
+	"""
+	r   = np.array([0.,0.,0.])
+	cou = 0
+	for atom in atomList:
+		r   += atom.coordinates
+		cou += 1
+	return r/cou
+
+
 def getPeriodicTestBox_molecules(Molecules,boxLength,numbershells=1):
 	vectors = []
 	for l in range(-numbershells,numbershells+1):
@@ -885,6 +1042,29 @@ def countHbonds_orig(mol1,mol2, Roocut=3.6, Rohcut=2.4, Aoooh=30.0):
 	abonds = hbnoA1 + hbnoA2
 	return dbonds, abonds
 
+def getTetraParameter(o_atoms,boxLength=None):
+	"""
+	according to NATURE, VOL 409, 18 JANUARY 2001
+	"""
+	tetra_params = []
+	for atom1 in o_atoms:
+		NN_dists = []
+		NN_atoms = []
+		for atom2 in o_atoms:
+			NN_dists.append(np.linalg.norm(atom1.coordinates - atom2.coordinates))
+		order = np.argsort(NN_dists)
+		for ii in order[1:5]:
+			NN_atoms.append(o_atoms[ii])
+		tetra_param = 0
+		for j in range(0,3):
+			for k in range(j+1,4):
+				vec1 = getDistVectorPbc(atom1,NN_atoms[j],boxLength)
+				vec2 = getDistVectorPbc(atom1,NN_atoms[k],boxLength)
+				psi = np.arccos(np.dot(vec1,vec2)/np.linalg.norm(vec1)/np.linalg.norm(vec2))
+				tetra_param += (np.cos(psi) + 1./3.)**2
+		tetra_params.append(1-3./8.*tetra_param)
+	return tetra_params
+
 def repair_h2o_molecules_pbc(h2o_mols,boxLength):
 	new_mols = []
 	for mol in h2o_mols:
@@ -1013,6 +1193,103 @@ def boxParser(filename):
 
         return xyzBox(xyzAtoms)
 
+def groBoxParser(filename,nanoMeter=True):
+	""" **groBoxParser**
+	Parses an gromacs GRO-style file for the xyzBox class.
+	"""
+	atoms       = []
+	coordinates = []
+	boxLength   = None
+	if nanoMeter:
+		scale = 10.0
+	else:
+		scale = 1.0
+	xyz     = open(filename)
+	title   = xyz.readline()
+	n_atoms = int(xyz.readline())
+	for line in xyz:
+		if len(line.split()) == 9:
+			name = line.split()[1][0]
+			x = float(line.split()[3])*scale
+			y = float(line.split()[4])*scale
+			z = float(line.split()[5])*scale
+			atoms.append(name)
+			coordinates.append([x, y, z])
+		elif len(line.split()) == 8:
+			name = line.split()[1][0]
+			x = float(line.split()[2])*scale
+			y = float(line.split()[3])*scale
+			z = float(line.split()[4])*scale
+			atoms.append(name)
+			coordinates.append([x, y, z])
+		elif len(line.split()) == 3:
+			boxLength = float(line.split()[0])*scale
+		else:
+			print('Something is fishy!')
+	xyz.close()
+	AllxyzAtoms = []
+	for ii in range(n_atoms):
+		AllxyzAtoms.append(xyzAtom(atoms[ii],coordinates[ii],ii))
+
+	return xyzBox(AllxyzAtoms,boxLength=boxLength)
+
+def groTrajecParser(filename,nanoMeter=True):
+	""" **groTrajecParser**
+	Parses an gromacs GRO-style file for the xyzBox class.
+	"""
+	boxes       = []
+	atoms       = []
+	coordinates = []
+	boxLength   = None
+	if nanoMeter:
+		scale = 10.0
+	else:
+		scale = 1.0
+	# read the header
+	xyz     = open(filename)
+	title   = xyz.readline()
+	n_atoms = int(xyz.readline())
+	xyz.close()
+	# how many lines per box
+	headerlines = 2
+	taillines   = 1
+	linesPerBox = headerlines + n_atoms + taillines
+	# read the rest
+	xyz     = open(filename)
+	counter = 0
+	for line in xyz:
+		if counter%linesPerBox == 0:
+			# new box 
+			title = line
+			atoms = []
+			coordinates = []
+		if counter%linesPerBox == 1:
+			n_atoms = int(line)
+		if counter in range(linesPerBox)[2::]:
+			if len(line.split()) == 9:
+				atoms.append(line.split()[1][0])
+				x = float(line.split()[3])*scale
+				y = float(line.split()[4])*scale
+				z = float(line.split()[5])*scale
+				coordinates.append([x, y, z])
+			elif len(line.split()) == 8:
+				atoms.append(line.split()[1][0])
+				x = float(line.split()[2])*scale
+				y = float(line.split()[3])*scale
+				z = float(line.split()[4])*scale
+				coordinates.append([x, y, z])
+		if counter > 1 and counter%(linesPerBox-1) == 0:
+			boxLength = float(line.split()[0])*scale
+			AllxyzAtoms = []
+			for ii in range(n_atoms):
+				AllxyzAtoms.append(xyzAtom(atoms[ii],coordinates[ii],ii))
+			boxes.append(xyzBox(AllxyzAtoms,boxLength=boxLength))
+			counter = -1
+		counter += 1
+	return boxes
+
+
+
 def changeOHBondLength(h2oMol, fraction, boxLength=None, oName='O', hName='H'):
 	o_atom = h2oMol.get_atoms_by_name(oName)
 	h_atom = h2oMol.get_atoms_by_name(hName)
@@ -1036,14 +1313,6 @@ def changeOHBondLength(h2oMol, fraction, boxLength=None, oName='O', hName='H'):
 	return xyzMolecule(newmol)
 
 
-class xyzTrajectory:
-        def __init__(self,xyzBoxes):
-                self.xyzBoxes = xyzBoxes
-
-        def writeRandBox(self,filename):
-                ind = np.random.randint(len(self.xyzBoxes))
-                self.xyzBoxes[ind].writeBox(filename)
-
 def parseXYZfile(filename):
         """**parseXYZfile**
         Reads an xyz-style file.
@@ -1059,6 +1328,15 @@ def parseXYZfile(filename):
                 coordinates.append([float(x), float(y), float(z)])
         xyz.close()
         return n_atoms, title, atoms, coordinates
+
+def alterGROatomNames(filename,oldName, newName):
+	import re
+	with open(filename, "r") as sources:
+		lines = sources.readlines()
+	with open(filename, "w") as sources:
+		for line in lines:
+			sources.write(re.sub(r'%s'%oldName, '%s'%newName, line))
+
 
 
 

XRStools/xrs_extraction.py
@@ -174,13 +174,22 @@ class edge_extraction:
 		self.avqvals    = np.array([])
 
 		# rough normalization over range given by prenormrange
+		scales = []
 		if prenormrange:
 			for n in range(self.signals.shape[1]):
 				HFnorm  = np.trapz(self.HF_dataset.J_total[:,n], self.eloss)
 				inds    = np.where(np.logical_and(self.eloss >= prenormrange[0], self.eloss <= prenormrange[1]))[0]
 				EXPnorm = np.trapz(self.signals[inds,n], self.eloss[inds])
-				self.signals[:,n] = self.signals[:,n]/EXPnorm*HFnorm
-				self.errors[:,n]  = self.errors[:,n]/EXPnorm*HFnorm
+				scales.append(EXPnorm*HFnorm)
+				#print 'HFnorm = ', HFnorm, ', EXPnorm = ', EXPnorm
+				#self.signals[:,n] /= EXPnorm
+				#self.signals[:,n] *= HFnorm
+				#self.errors[:,n]  /= EXPnorm
+				#self.errors[:,n]  *= HFnorm
+			scale = np.mean(scales)
+			for n in range(self.signals.shape[1]):
+				self.signals[:,n] *= scale
+				self.errors[:,n]  *= scale
 
 	def analyzerAverage(self,roi_numbers,errorweighing=True):
 		"""
@@ -234,8 +243,8 @@ class edge_extraction:
 
 		# sum things up
 		if errorweighing:
-			self.avsignals = np.sum( av/averr**2.0 ,axis=1)/( np.sum(1.0/averr**2.0,axis=1))
-			self.averrors  = np.sqrt( 1.0/np.sum(1.0/(averr)**2.0,axis=1) )
+			self.avsignals = np.sum( av/(averr**2.0) ,axis=1)/( np.sum(1.0/(averr**2.0),axis=1))
+			self.averrors  = np.sqrt( 1.0/np.sum(1.0/(averr**2.0),axis=1) )
 		else: 
 			self.avsignals = np.sum(av,axis=1)
 			self.averrors  = np.sqrt(np.sum(np.absolute(averr)**2.0,axis=1)) # check this again

XRStools/xrs_utilities.py
@@ -52,7 +52,7 @@ from scipy.integrate import trapz
 from scipy import interpolate, signal, integrate, constants, optimize
 from re import findall
 from scipy.ndimage import measurements
-from scipy.optimize import leastsq, fmin, fsolve
+from scipy.optimize import leastsq, fmin, fsolve, minimize
 from scipy.interpolate import Rbf, RectBivariateSpline
 from scipy.integrate import odeint
 
@@ -721,16 +721,11 @@ def cixsUBgetAngles(Q, G):
 	Q_lab = np.linalg.lstsq(U,v_c)[0]
 	
 	#$[angles,FVAL,EXITFLAG,OUTPUT] = fsolve(@(x) UBfind(x, G, Q_lab), [0 45 0]);
-	lab_angles = optimize.fsolve(cixsUBfind, [13.5, 25.0, 155.0], args=(G,Q_lab,wi,wo,lambdai,lambdao))
+	lab_angles = optimize.fsolve(cixsUBfind, [20.5, 15.0, 5.0], args=(G,Q_lab,wi,wo,lambdai,lambdao))
 
 	tthv = lab_angles[1]
 	tthh = lab_angles[0]
 	psi  = lab_angles[2]
-	if psi <= -360.0:
-		psi += 360.0
-	if psi >= 360.0:
-		psi -= 360.0
-
 	return tthv, tthh, psi
 
 def cixsUBgetAngles_secondo(Q, G):
@@ -779,7 +774,7 @@ def cixsUBgetAngles_secondo(Q, G):
 	Q_lab = np.linalg.lstsq(U,v_c)[0]
 	
 	#$[angles,FVAL,EXITFLAG,OUTPUT] = fsolve(@(x) UBfind(x, G, Q_lab), [0 45 0]);
-	lab_angles = optimize.fsolve(cixsUBfind, [30.5, 40.0, 280.0], args=(G,Q_lab,wi,wo,lambdai,lambdao), xtol=1.49012e-12,maxfev=1000000)
+	lab_angles = optimize.fsolve(cixsUBfind, [25.5, 0.0, 0.0], args=(G,Q_lab,wi,wo,lambdai,lambdao), xtol=1.49012e-12,maxfev=1000000)
 
 	tthv = lab_angles[1]
 	tthh = lab_angles[0]
@@ -839,6 +834,120 @@ def cixsUBgetQ_secondo(tthv, tthh, psi, G):
 	Q_test = np.dot(np.linalg.lstsq(B,U)[0],vrot(Ki_test-Ko_test,v,-psi))
 	return Q_test
 
+
+def cixsUBgetAngles_terzo(Q, G):
+	# incoming/outgoing energy/wavelength
+	hc = 12.3984191
+	bragg_ang = 86.5
+	wo = energy(dspace([4, 4, 4]),bragg_ang)
+	lambdao = hc/wo
+	wi = wo
+	lambdai = hc/wi
+
+	# lattice parameters
+	lattice = np.array([5.43095, 5.43095, 5.43095])
+	angles  = np.radians(np.array([90.0, 90.0, 90.0])) # in radians !!!
+	a = np.array([lattice[0], 0, 0])
+	b = np.array([lattice[0]*np.cos(angles[2]), lattice[1]*np.sin(angles[2]), 0])
+	c = np.array([lattice[2]*np.cos(angles[1]), lattice[2]*(-np.cos(angles[1])*np.arctan(angles[2])+np.cos(angles[0])*(1.0/np.sin(angles[2]))), lattice[2]/np.sqrt(2.0)*np.sqrt((1.0/np.sin(angles[2]))*((4.0*np.cos(angles[0])*np.cos(angles[1])*np.arctan(angles[2])-(1.0 + np.cos(2.0*angles[0])+np.cos(2.0*angles[1])+np.cos(2.0*angles[2]))*(1.0/np.sin(angles[2])))))])
+
+	# lab-to-sample reference system transformation matrix U for Si220-crystal
+	th = braggd(G,wo)
+	xxx = vrot(np.array([0.0,-1.0,1.0]),np.array([-2.0,1.0,1.0]),th)
+	yyy = vrot(np.array([-2.0,1.0,1.0]),np.array([-2.0,1.0,1.0]),th)
+	zzz = vrot(G,np.array([-2.0,1.0,1.0]),th)
+	#xxx = vrot(np.array([0.0,1.0,-1.0]),np.array([2.0,-1.0,-1.0]),th)
+	#yyy = vrot(np.array([2.0,-1.0,-1.0]),np.array([2.0,-1.0,-1.0]),th)
+	#zzz = vrot(G,np.array([2.0,-1.0,-1.0]),th)
+	U = np.zeros((3,3))
+	U[:,0] = xxx/np.linalg.norm(xxx)
+	U[:,1] = yyy/np.linalg.norm(yyy)
+	U[:,2] = zzz/np.linalg.norm(zzz)
+
+	# reciprocal lattice to absolute units transformation matrix
+	a_star = 2.0*np.pi*np.cross(b,c)/np.dot(a,np.cross(b,c))
+	b_star = 2.0*np.pi*np.cross(c,a)/np.dot(a,np.cross(b,c))
+	c_star = 2.0*np.pi*np.cross(a,b)/np.dot(a,np.cross(b,c))
+	angles_star = np.array([np.arccos(np.dot(b_star,c_star)/np.linalg.norm(b_star)/np.linalg.norm(c_star)), np.arccos(np.dot(c_star,a_star)/np.linalg.norm(c_star)/np.linalg.norm(a_star)), np.arccos(np.dot(a_star,b_star)/np.linalg.norm(a_star)/np.linalg.norm(b_star))])
+	B = np.zeros((3,3))
+	B[:,0] = np.array([np.linalg.norm(a_star), np.linalg.norm(b_star)*np.cos(angles_star[2]), np.linalg.norm(c_star)*np.cos(angles_star[1])])
+	B[:,1] = np.array([0.0, np.linalg.norm(b_star)*np.sin(angles_star[2]), -np.linalg.norm(c_star)*np.sin(angles_star[1])*np.cos(angles[0])])
+	B[:,2] = np.array([0.0, 0.0, 2.0*np.pi/np.linalg.norm(c)])
+
+	# laboratory reference frame
+	X = np.array([1.0, 0.0, 0.0])
+	Y = np.array([0.0, 1.0, 0.0])
+	Z = np.array([0.0, 0.0, 1.0])
+
+	# desired momentum in the laboratory reference system before any rotation is applied
+	v_c = np.dot(B,Q)
+	Q_lab = np.linalg.lstsq(U,v_c)[0]
+	
+	#$[angles,FVAL,EXITFLAG,OUTPUT] = fsolve(@(x) UBfind(x, G, Q_lab), [0 45 0]);
+	lab_angles = optimize.fsolve(cixsUBfind, [15., 20.0, 0.0], args=(G,Q_lab,wi,wo,lambdai,lambdao), xtol=1.49012e-12,maxfev=1000000)
+
+	tthv = lab_angles[1]
+	tthh = lab_angles[0]
+	psi  = lab_angles[2]
+	#if psi <= -360.0:
+	#	psi += 360.0
+	#if psi >= 360.0:
+	#	psi -= 360.0
+
+	return tthv, tthh, psi
+
+def cixsUBgetQ_terzo(tthv, tthh, psi, G):
+	# incoming/outgoing energy/wavelength
+	hc = 12.3984191
+	bragg_ang = 86.5
+	wo = energy(dspace([4., 4., 4.]),bragg_ang)
+	lambdao = hc/wo
+	wi = wo
+	lambdai = hc/wi
+
+	# lattice parameters
+	lattice = np.array([5.43095, 5.43095, 5.43095])
+	angles  = np.radians(np.array([90.0, 90.0, 90.0])) # in radians !!!
+	a = np.array([lattice[0], 0, 0])
+	b = np.array([lattice[0]*np.cos(angles[2]), lattice[1]*np.sin(angles[2]), 0])
+	c = np.array([lattice[2]*np.cos(angles[1]), lattice[2]*(-np.cos(angles[1])*np.arctan(angles[2])+np.cos(angles[0])*(1.0/np.sin(angles[2]))), lattice[2]/np.sqrt(2.0)*np.sqrt((1.0/np.sin(angles[2]))*((4.0*np.cos(angles[0])*np.cos(angles[1])*np.arctan(angles[2])-(1.0 + np.cos(2.0*angles[0])+np.cos(2.0*angles[1])+np.cos(2.0*angles[2]))*(1.0/np.sin(angles[2])))))])
+
+	# lab-to-sample reference system transformation matrix U
+	th = braggd(G,wo)
+	xxx = vrot(np.array([0.0,-1.0,1.0]),np.array([-2.0,1.0,1.0]),th)
+	yyy = vrot(np.array([-2.0,1.0,1.0]),np.array([-2.0,1.0,1.0]),th)
+	zzz = vrot(G,np.array([-2.0,1.0,1.0]),th)
+	#xxx = vrot(np.array([0.0,1.0,-1.0]),np.array([2.0,-1.0,-1.0]),th)
+	#yyy = vrot(np.array([2.0,-1.0,-1.0]),np.array([2.0,-1.0,-1.0]),th)
+	#zzz = vrot(G,np.array([2.0,-1.0,-1.0]),th)
+	U = np.zeros((3,3))
+	U[:,0] = xxx/np.linalg.norm(xxx)
+	U[:,1] = yyy/np.linalg.norm(yyy)
+	U[:,2] = zzz/np.linalg.norm(zzz)
+
+	# reciprocal lattice to absolute units transformation matrix
+	a_star = 2.0*np.pi*np.cross(b,c)/np.dot(a,np.cross(b,c))
+	b_star = 2.0*np.pi*np.cross(c,a)/np.dot(a,np.cross(b,c))
+	c_star = 2.0*np.pi*np.cross(a,b)/np.dot(a,np.cross(b,c))
+	angles_star = np.array([np.arccos(np.dot(b_star,c_star)/np.linalg.norm(b_star)/np.linalg.norm(c_star)), np.arccos(np.dot(c_star,a_star)/np.linalg.norm(c_star)/np.linalg.norm(a_star)), np.arccos(np.dot(a_star,b_star)/np.linalg.norm(a_star)/np.linalg.norm(b_star))])
+	B = np.zeros((3,3))
+	B[:,0] = np.array([np.linalg.norm(a_star), np.linalg.norm(b_star)*np.cos(angles_star[2]), np.linalg.norm(c_star)*np.cos(angles_star[1])])
+	B[:,1] = np.array([0.0, np.linalg.norm(b_star)*np.sin(angles_star[2]), -np.linalg.norm(c_star)*np.sin(angles_star[1])*np.cos(angles[0])])
+	B[:,2] = np.array([0.0, 0.0, 2.0*np.pi/np.linalg.norm(c)])
+
+	# laboratory reference frame
+	X = np.array([1.0, 0.0, 0.0])
+	Y = np.array([0.0, 1.0, 0.0])
+	Z = np.array([0.0, 0.0, 1.0])
+
+	# axis of rotation of psi
+	v = np.array([-np.sin(np.radians(th)), 0.0, np.cos(np.radians(th))])
+	Ki_test = 2.0*np.pi/lambdai*X
+	Ko_test = 2.0*np.pi/lambdao*vrot(vrot(X,Y,-tthv) ,Z, tthh)
+	Q_test = np.dot(np.linalg.lstsq(B,U)[0],vrot(Ki_test-Ko_test,v,-psi))
+	return Q_test
+
+
 def cixsUBfind(x,G,Q_sample,wi,wo,lambdai,lambdao):
 	""" **cixsUBfind**
 	"""	
@@ -853,7 +962,7 @@ def cixsUBfind(x,G,Q_sample,wi,wo,lambdai,lambdao):
 	Q = Ki-Ko
 	th = braggd(G,wo)
 	v  = np.array([-np.sin(np.radians(th)), 0.0, np.cos(np.radians(th))])
-	y = Q - vrot(Q_sample, v, -psi)
+	y = Q - vrot(Q_sample, v, psi)
 	tthh = y[0]
 	tthv = y[1]
 	psi  = y[2]
@@ -1053,6 +1162,71 @@ def constrained_nnmf(A,W_ini,H_ini,W_up,H_up,max_iter=10000,verbose=False):
 	return W, H
 
 
+
+
+def mat2con(W,H,W_up,H_up):
+	x = W[W_up == 1]
+	x = np.append(x, H[H_up == 1])
+	return x
+
+def con2mat(x,W,H,W_up,H_up):
+	W[W_up == 1] = x[0:len(W[W_up == 1])]
+	H[H_up == 1] = x[len(W[W_up == 1]):len(W[W_up == 1])+len(H[H_up == 1])]
+	return W, H
+
+def NNMFcost(x,A,W,H,W_up,H_up):
+	""" **NNMFcost**
+	Returns cost and gradient for NNMF with constraints.
+	"""
+	# calculate W, H
+	W, H = con2mat(x,W,H,W_up,H_up)
+	# calculate cost and gradient
+	J = np.sum(np.sum(0.5*(A-np.dot(W,H))*(A-np.dot(W,H))))
+	gradW = -(np.dot((A-np.dot(W,H)),H.T))
+	gradH = -(np.dot((A-np.dot(W,H)).T,W)).T
+	# return constraint only for updates
+	xgrad = mat2con(gradW,gradH,W_up,H_up)
+	return J, xgrad
+
+def bootstrapCNNMF(A,k,Aerr, F_ini, C_ini, F_up, C_up, Niter=100):
+	""" **bootstrapCNNMF**
+	Constrained non-negative matrix factorization with bootstrapping
+	for error estimates.
+	"""
+	n,m = A.shape
+	import copy
+	F1s = np.zeros((Niter,n,k))
+	C1s = np.zeros((Niter,C_ini.shape[0],C_ini.shape[1]))
+	for ii in range(Niter):
+		A1 = copy.deepcopy(A)
+		# add random noise
+		A1 += np.random.random((n,m))*Aerr
+		F1 = F_ini*(1.0-F_up) + F_up*np.random.random((n,k))
+		C1 = C_ini*(1.0-C_up) + C_up*np.random.random((k,m))
+		F1[F1<0.0]=0.0
+		C1[C1<0.0]=0.0
+		# minimize with trust-region-algorithm
+		# get starting values
+		x0 = mat2con(F1,C1,F_up,C_up)
+		cons = ({'args': (A1,F1,C1,F_up,C_up)})
+		bnds = [(0.0,1.0) for ii in x0]
+		costfun = lambda x:NNMFcost(x,A1,F1,C1,F_up,C_up) #[0]
+		#gradfun = lambda x:NNMFcost(x,A1,F1,C1,F_up,C_up)[1]
+		x=minimize(NNMFcost,x0,args=(A1,F1,C1,F_up,C_up), method='Newton-CG', tol=1e-5, jac=True, bounds=bnds,options={'maxiter' : 1e6, 'disp': True} ).x
+		Fbs1, Cbs1 = con2mat(x,F1,C1,F_up,C_up)
+		# store meaningful data 
+		print F1s.shape
+		print C1s.shape
+		F1s[ii,:,:] = Fbs1/(np.dot(np.ones((np.shape(Fbs1)[0],1)), np.sum(Fbs1,axis=0).reshape(1,len(np.sum(Fbs1,axis=0))) ))
+		C1s[ii,:,:] = Cbs1*(np.dot(np.ones( (Cbs1.shape[1],1) ),sum(Fbs1))).T
+	# do RMS
+	print F1s.shape, C1s.shape
+	Cerr=np.squeeze(np.std(C1s,axis=0))
+	Ferr=np.squeeze(np.std(F1s,axis=0))
+	# average
+	C=np.squeeze(np.mean(C1s,axis=0))
+	F=np.squeeze(np.mean(F1s,axis=0))
+	return F, C, Ferr, Cerr
 
 
 